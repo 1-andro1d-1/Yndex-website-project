37bb0fb6f44774546af46cf7cc96c5bb4e0360ee
asmar <andrew.bakradze@internet.com>
2023-04-13 10:29:04 +0900

    login and register

--- main.py
+++ main.py
@@ -1,27 +1,58 @@
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, url_for
+from flask_login import LoginManager, login_user, login_required, current_user
 from data import db_session
 from data.users import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from UserLogin import UserLogin
+from somefunc import *
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'andrew-bakradze'
+login_manager = LoginManager(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    print('load_user')
+    return UserLogin().fromDB(user_id)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if current_user.is_authenticated:
+        return render_template('index2.html', title=str(current_user.get_name()))
+    else:
+        return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def login():
+    if request.method == 'POST':
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == request.form['email']).first()
+        print(user)
+        if user and check_password_hash(user.hashed_password, request.form['password']):
+            userLogin = UserLogin().create(user)
+            login_user(userLogin, remember=True)
+            return redirect(url_for('index'))
     return render_template('index3.html')
 
 
 @app.route('/register', methods=["POST", "GET"])
+
 def register():
     if request.method == 'POST':
-        pass
+        if len(request.form['name']) > 4 and len(request.form['password']) > 4:
+            hash = generate_password_hash(request.form['password'])
+            user = User()
+            user.name = request.form['name']
+            user.hashed_password = hash
+            user.courses = 'none'
+            user.email = request.form['email']
+            db_sess = db_session.create_session()
+            db_sess.add(user)
+            db_sess.commit()
+            print('123')
+            return redirect(url_for('index'))
     return render_template('index4.html')
 
 
